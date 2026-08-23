41d32ee9b9ba86738f98f998e9885522c969f274
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-08-23 17:49:18 +0200

    Load SwearJar reset stats command

--- swearjar/__init__.py
+++ swearjar/__init__.py
@@ -1,5 +1,7 @@
 from .swearjar import SwearJar
+from .resetstats import SwearJarResetStats
 
 
 async def setup(bot):
     await bot.add_cog(SwearJar(bot))
+    await bot.add_cog(SwearJarResetStats(bot))
